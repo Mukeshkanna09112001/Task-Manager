b38a05a98165852df49fc837dcfa1d05d5138f84
Mukesh Kanna <101079315+Mukeshkanna09112001@users.noreply.github.com>
2025-07-08 19:05:43 +0530

    Delete src directory

--- src/routes/userRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getUsers } from '../controllers/userController';
-import { protect, isAdmin } from '../middleware/authMiddleware';
-
-const router = express.Router();
-
-router.get('/', protect, getUsers);
-
-export default router;
